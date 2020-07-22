129c8df318270b792873dd4a705195310c8bedd3
Davide Berdin <davideberdin@gmail.com>
2020-07-22 18:45:57 +0200

    remove output

--- outputs.tf
+++
@@ -1,5 +0,0 @@
-// used to create eventual dependencies for other modules that need to install other components
-// for example: secrets for ingress authentication
-output "namespace_name" {
-  value = "${kubernetes_namespace.linkerd[0].metadata.0.name}"
-}
